c816c3f4e4b800b9870f30390c736230167e2e15
Daniel Heater <daniel@heatertech.com>
2014-05-18 20:33:00 -0500

    Submit assignment 1 part 1-4

---
+++ R_programming/assignment_1/pollutantmean.R
@@ -0,0 +1,32 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+
+    ## 'pollutant' is a character vector of length 1 indicating
+    ## the name of the pollutant for which we will calculate the
+    ## mean; either "sulfate" or "nitrate".
+
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+
+    ## Return the mean of the pollutant across all monitors list
+    ## in the 'id' vector (ignoring NA values)
+    df = data.frame(Date=as.Date(character()),
+                    sulfate=as.numeric(),
+                    nitrate=as.numeric(),
+                    id=integer())
+
+    for(i in id){
+        fname = sprintf("%s/%03d.csv", directory, i)
+        sample = read.csv(fname)
+        df = rbind(df, sample)
+    }
+    if("nitrate" == pollutant) {
+        mean(df$nitrate, na.rm = TRUE)
+    } else if("sulfate" == pollutant) {
+        mean(df$sulfate, na.rm = TRUE)
+    } else {
+        print("Do what?")
+        NA
+    }
+}
